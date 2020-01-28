1cf71446bbac487f8f16ba923c9791722c6591cc
cidwells <cidwells@gmail.com>
2020-01-28 18:10:01 -0500

    alibaba 2 tests complete

---
+++ Alibaba/src/main/java/home/AlibabaSearch.java
@@ -0,0 +1,17 @@
+package home;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class AlibabaSearch extends CommonAPI {
+
+//    @FindBy(how = How.CSS, using = "locator")
+//    public static WebElement kitchenWebElement;
+
+
+//    public static WebElement getKitchenWebElement(){
+//        return getKitchenWebElement();
+//    }
+}
