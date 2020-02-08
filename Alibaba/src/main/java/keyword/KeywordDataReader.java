ce233e1fd42eadfbbba6b9c884a0a2d4d16b332e
cidwells <cidwells@gmail.com>
2020-02-08 08:40:28 -0500

    keyword driven test still broken

--- Alibaba/src/main/java/keyword/KeywordDataReader.java
+++ Alibaba/src/main/java/keyword/KeywordDataReader.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class KeywordDataReader extends CommonAPI {
     DataReader dr = new DataReader();
     public String[] getDataFromExcelFile() throws IOException {
-        String path = System.getProperty("user.dir")+"Alibaba/KeywordData/Book.xls";
+        String path = System.getProperty("user.dir")+"/KeywordData/Book.xls";
         String [] data = dr.fileReader2(path,0);
         return data;
     }
